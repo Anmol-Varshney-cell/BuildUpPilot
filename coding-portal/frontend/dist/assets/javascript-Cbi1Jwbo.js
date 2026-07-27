d6914f06a1cc846f776035639b10f2e265f66aae
Anmol <anmol@example.com>
2026-07-27 23:20:38 +0530

    Package complete production Skill Up React frontend bundle with Monaco editor workers

--- coding-portal/frontend/dist/assets/javascript-Cbi1Jwbo.js
+++ coding-portal/frontend/dist/assets/javascript-Cbi1Jwbo.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-CKPPoz5A.js";import"./index-DG6FQ4Yv.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-DsiWuBz0.js";import"./index-Cu-TZLx6.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
